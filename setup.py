99d778ed6cac6bd092c92deb13ca6b11658f8f82
JJMC89 <JJMC89@users.noreply.github.com>
2018-06-16 18:28:03 -0700

    Add setup

---
+++ setup.py
@@ -0,0 +1,45 @@
+"""Installer script."""
+from setuptools import find_packages, setup
+
+def readme():
+    """Read the readme file."""
+    with open('README.rst') as file:
+        return file.read()
+
+setup(
+    author='JJMC89',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Wiki',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    dependency_links=[
+        'https://github.com/earwig/mwparserfromhell/tarball/master#egg=mwparserfromhell',
+        'https://github.com/wikimedia/pywikibot/tarball/master#egg=pywikibot'
+    ],
+    description='BSicons bot',
+    download_url='https://github.com/JJMC89/bsiconsbot',
+    include_package_data=True,
+    install_requires=[
+        'mwparserfromhell',
+        'pywikibot'
+    ],
+    keywords=['bot', 'bsicon', 'mediawiki'],
+    license='MIT',
+    long_description=readme(),
+    name='bsiconsbot',
+    packages=find_packages(exclude=['tests']),
+    project_urls={
+        'Bug tracker': 'https://github.com/JJMC89/bsiconsbot/issues',
+        'Source code': 'https://github.com/JJMC89/bsiconsbot',
+    },
+    version='0.1.dev1',
+)
